cc4d7b2f1ce2a0cb8ad5f63b8ae4595e807310be
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-08-15 19:26:48 -0400

    add class dimProductSubcategory

---
+++ Api/Domain/Product/DimProductSubcategory.cs
@@ -0,0 +1,35 @@
+﻿namespace Api.Domain.Product
+{
+    public class DimProductSubcategory
+    {
+        public int ProductSubcategoryKey { get; private set; }
+        public string ProductSubcategoryLabel { get; private set; }
+        public string ProductSubcategoryName { get; private set; }
+        public string ProductSubcategoryDescription { get; private set; }
+        public int ProductCategoryKey { get; private set; }
+        public int ETLLoadID { get; private set; }
+        public DateTime LoadDate { get; private set; }
+        public DateTime UpdateDate { get; private set; }
+
+        public DimProductSubcategory(
+            int productSubcategoryKey,
+            string productSubcategoryLabel,
+            string productSubcategoryName,
+            string productSubcategoryDescription,
+            int productCategoryKey,
+            int etlLoadID,
+            DateTime loadDate,
+            DateTime updateDate)
+        {
+            ProductSubcategoryKey = productSubcategoryKey;
+            ProductSubcategoryLabel = productSubcategoryLabel;
+            ProductSubcategoryName = productSubcategoryName;
+            ProductSubcategoryDescription = productSubcategoryDescription;
+            ProductCategoryKey = productCategoryKey;
+            ETLLoadID = etlLoadID;
+            LoadDate = loadDate;
+            UpdateDate = updateDate;
+        }
+    }
+
+}
